3264fba7c4c0b9878b82eb37d90eadb6072af680
Mejbah <mejba.s@gmail.com>
2022-04-17 22:50:22 +0600

    Register button added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h2>Register</h2>
+        <div className='register-form'>
+            <h2 className='text-center mt-3'>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name='name' id='' placeholder='Your Name' />
+                <input type="email" name='email' id='' placeholder=' Email Address' required />
+                <input type="password" name='password' id='' placeholder='Inter Password' required />
+                <input className='bg-dark text-light rounded w-50 register-button' type="submit" value="Register" />
+            </form>
+            <p className='text-center'>Already have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
