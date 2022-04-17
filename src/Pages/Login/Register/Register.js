130e38b8ea6eec9a0e3edfac08a46f016625bb6c
Mejbah <mejba.s@gmail.com>
2022-04-17 22:07:49 +0600

    Create Login form and activate by using useRef

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Login = () => {
+const Register = () => {
     return (
         <div>
-            <h2>This is Login</h2>
+            <h2>Register</h2>
         </div>
     );
 };
 
-export default Login;
+export default Register;
