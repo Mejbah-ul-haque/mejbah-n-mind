8d13e5e568fbaf6f63cc847fedeb111260f36116
Mejbah <mejba.s@gmail.com>
2022-04-18 00:10:29 +0600

    set environment variable for firebase config

--- src/firebase.inti.js
+++ src/firebase.inti.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBKJnqn-vVCMXaBH0sjekgJW6ZEFJomrw8",
-    authDomain: "mejbah-n-mind.firebaseapp.com",
-    projectId: "mejbah-n-mind",
-    storageBucket: "mejbah-n-mind.appspot.com",
-    messagingSenderId: "837251878233",
-    appId: "1:837251878233:web:ecbc14ff093ff8c34d1048"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
