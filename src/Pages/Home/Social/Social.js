50b96b531d5908b6e0fe1088ce1ff92f710e759b
Mejbah <mejba.s@gmail.com>
2022-10-01 04:14:22 +0600

    Logo and others update

--- src/Pages/Home/Social/Social.js
+++ src/Pages/Home/Social/Social.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Linkedin from "../../../Images/Social/linkedin.png";
+import GitHub from "../../../Images/Social/github.png";
+import Facebook from "../../../Images/Social/facebook.png";
+import Instagram from "../../../Images/Social/Instagram.png";
+import Twitter from "../../../Images/Social/twitter.png";
+import Behance from "../../../Images/Social/behance.png";
+
 import "./Social.css";
 
 const Social = () => {
@@ -10,7 +16,7 @@ const Social = () => {
           <div className=" shadow-lg">
             <a className='text-decoration-none' target="_blank" href="https://github.com/Mejbah-ul-haque">
             <div className="d-flex justify-content-around align-items-center social-one">
-            <img className="img-fluid w-25" src={Linkedin} alt=""></img>
+            <img className="img-fluid w-25" src={GitHub} alt=""></img>
             <h4 className=" py-2">GitHub</h4>
             </div>
             </a>
@@ -30,7 +36,7 @@ const Social = () => {
           <div className=" shadow-lg">
             <a className='text-decoration-none' target="_blank" href="https://www.behance.net/mejbahulhaque">
             <div className="d-flex justify-content-around align-items-center social-one">
-            <img className="img-fluid w-25" src={Linkedin} alt=""></img>
+            <img className="img-fluid w-25" src={Behance} alt=""></img>
             <h4 className=" py-2">Behance</h4>
             </div>
             </a>
@@ -40,7 +46,7 @@ const Social = () => {
           <div className=" shadow-lg">
             <a className='text-decoration-none' target="_blank" href="https://twitter.com/mejba_s">
             <div className="d-flex justify-content-around align-items-center social-one">
-            <img className="img-fluid w-25" src={Linkedin} alt=""></img>
+            <img className="img-fluid w-25" src={Twitter} alt=""></img>
             <h4 className=" py-2">Twitter</h4>
             </div>
             </a>
@@ -50,7 +56,7 @@ const Social = () => {
           <div className=" shadow-lg">
             <a className='text-decoration-none' target="_blank" href="https://www.facebook.com/profile.php?id=100000490430612">
             <div className="d-flex justify-content-around align-items-center social-one">
-            <img className="img-fluid w-25" src={Linkedin} alt=""></img>
+            <img className="img-fluid w-25" src={Facebook} alt=""></img>
             <h4 className=" py-2">Facebook</h4>
             </div>
             </a>
@@ -60,7 +66,7 @@ const Social = () => {
           <div className=" shadow-lg">
             <a className='text-decoration-none' target="_blank" href="https://www.instagram.com/mejbah_mcj/">
             <div className="d-flex justify-content-around align-items-center social-one">
-            <img className="img-fluid w-25" src={Linkedin} alt=""></img>
+            <img className="img-fluid w-25" src={Instagram} alt=""></img>
             <h4 className=" py-2">Instagram</h4>
             </div>
             </a>
