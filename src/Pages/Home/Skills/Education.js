0e5dbb5ea249c86d498baf2b38221229b7e16253
Mejbah <mejba.s@gmail.com>
2022-10-03 00:53:11 +0600

    new update

--- src/Pages/Home/Skills/Education.js
+++ src/Pages/Home/Skills/Education.js
@@ -2,6 +2,7 @@ import React from "react";
 import RU from "../../../../src/Images/Education/ru.png";
 import School from "../../../../src/Images/Education/school.png";
 import Bamandanga from "../../../../src/Images/Education/bamandanga.png";
+import Primary from "../../../../src/Images/Education/primary.png";
 
 
 const Education = () => {
@@ -10,37 +11,51 @@ const Education = () => {
 			<div class="row row-cols-2 row-cols-md-3 row-cols-xl-6 g-4">
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg ">
-						<img src={RU} class="card-img logo-imag pb-3" alt="..." />
+						<img src={RU} class="card-img logo-imag pb-3 pt-5" alt="..." />
 						<div class="card-img-overlay text-center lead-style pt-2">
 							<h5 class="card-title fw-bold text-info">MSS</h5>
-              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Rajshahi University</small></p>
+              <p className="border-top border-2 border-dark text-secondary pt-0 pb-1 my-0"><small>Rajshahi University</small></p>
+              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Mass Comm. & Journalism</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={RU} class="card-img logo-imag pb-3" alt="..." />
+						<img src={RU} class="card-img logo-imag pb-3 pt-5" alt="..." />
 						<div class="card-img-overlay text-center lead-style pt-2">
 							<h5 class="card-title fw-bold text-info">BSS <small className="fw-lighter text-secondary">(honors)</small></h5>
-              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Rajshahi University</small></p>
+              <p className="border-top border-2 border-dark text-secondary pt-0 pb-1 my-0"><small>Rajshahi University</small></p>
+              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Mass Comm. & Journalism</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={Bamandanga} class="card-img logo-imag pb-3" alt="..." />
+						<img src={Bamandanga} class="card-img logo-imag pb-3 pt-5" alt="..." />
 						<div class="card-img-overlay text-center lead-style pt-2">
 							<h5 class="card-title fw-bold text-info">HSC</h5>
-              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Bamandanga College</small></p>
+              <p className="border-top border-2 border-dark text-secondary pt-0 pb-1 my-0"><small>Bamandanga College</small></p>
+              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Science Group</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={School} class="card-img logo-imag pb-3" alt="..." />
+						<img src={School} class="card-img logo-imag pb-3 pt-5" alt="..." />
 						<div class="card-img-overlay text-center lead-style pt-2">
 							<h5 class="card-title fw-bold text-info">SSC</h5>
-              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Kathgora School</small></p>
+              <p className="border-top border-2 border-dark text-secondary pt-0 pb-1 my-0"><small>Kathgora School</small></p>
+              <p className="border-top border-2 border-dark pt-0 mt-0"><small>Science Group</small></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
+					<div class="card text-white skill-bg">
+						<img src={Primary} class="card-img logo-imag pb-3 pt-5" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fw-bold text-info">Primary</h5>
+              <p className="border-top border-2 border-dark text-secondary pt-0 pb-1 my-0"><small>Kathgora Govt. School</small></p>
+              <p className="border-top border-2 border-dark pt-0 mt-0"><small></small></p>
 						</div>
 					</div>
 				</div>
--- src/Pages/Home/Skills/Journalism.js
+++ src/Pages/Home/Skills/Journalism.js
@@ -1,91 +1,119 @@
 import React from "react";
-import SouthKorea from "../../../../src/Images/Traveling/South_Korea.png";
-import England from "../../../../src/Images/Traveling/England.png";
-import India from "../../../../src/Images/Traveling/India.png";
-import China from "../../../../src/Images/Traveling/china.png";
-import Scotland from "../../../../src/Images/Traveling/Scotland.png";
-import Wales from "../../../../src/Images/Traveling/Wales.png";
-import UAE from "../../../../src/Images/Traveling/UAE.png";
-import Oman from "../../../../src/Images/Traveling/Oman.png";
-import Australia from "../../../../src/Images/Traveling/Australia.png";
+
+import GolfMag1 from "../../../../src/Images/Journalism/golf_mag1.png";
+import GolfMag2 from "../../../../src/Images/Journalism/golf_mag2.png";
+import Security1 from "../../../../src/Images/Journalism/security1.png";
+import Security2 from "../../../../src/Images/Journalism/security2.png";
+import Pratidin from "../../../../src/Images/Journalism/pratidin.png";
+import NewsLetter from "../../../../src/Images/Journalism/news_letter.png";
+
 
 const Journalism = () => {
   return (
     <div>
 			<div class="row row-cols-2 row-cols-md-3 row-cols-xl-6 g-4">
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
-					<div class="card text-white skill-bg">
-						<img src={SouthKorea} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title fw-bold text-info border-bottom border-2 border-dark pb-3">South Korea</h5>
+					<div class="card text-white skill-bg ">
+						<img src={Pratidin} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Senior Reporter</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>Bangladesh <span className="text-success">Pratidin</span></small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>Daily Newspaper</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={England} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title text-warning border-bottom border-2 border-dark pb-3">England</h5>
+						<img src={GolfMag1} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Founder Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>Golf Bangladesh</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 my-0"><small>Magazine, v.1</small> </p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={India} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title fw-bold border-bottom border-2 border-dark pb-3" style={{color:"#418a3e"}}>India</h5>
+						<img src={GolfMag2} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>Golf Bangladesh</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>Magazine, v.2</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={China} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title fw-bold border-bottom border-2 border-dark pb-3" style={{color:"red"}}>China</h5>
+						<img src={GolfMag1} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>Golf Bangladesh</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>Magazine, v.3</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={Scotland} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title fw-bold border-bottom border-2 border-dark pb-3" style={{color:"#418a3e"}}>Scotland</h5>
+						<img src={Security1} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>The Security</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>Magazine, v.1</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={Wales} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title fw-bold border-bottom border-2 border-dark pb-3" style={{color:"orange"}}>Wales</h5>
+						<img src={Security2} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>The Security</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>Magazine, v.2</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={UAE} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title text-primary fw-bold border-bottom border-2 border-dark pb-3">UAE</h5>
+						<img src={GolfMag1} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>The Security</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>Magazine, v.3</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={Oman} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title text-info border-bottom border-2 border-dark pb-3">Oman</h5>
+						<img src={NewsLetter} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>Golf Bangladesh</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>News Letter, v.1</small></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
 					<div class="card text-white skill-bg">
-						<img src={Australia} class="card-img logo-imag pb-3" alt="..." />
-						<div class="card-img-overlay text-center lead-style">
-							<h5 class="card-title fw-bold border-bottom border-2 border-dark pb-3" style={{color:"#7e12f9"}}>Australia</h5>
+						<img src={NewsLetter} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>Golf Bangladesh</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>News Letter, v.2</small></p>
 						</div>
 					</div>
 				</div>
-				
+				<div class="col-xs-6 col-sm-6 col-md-3 col-xl-2 col-xxl-2">
+					<div class="card text-white skill-bg">
+						<img src={NewsLetter} class="card-img logo-imag pt-5 pb-2" alt="..." />
+						<div class="card-img-overlay text-center lead-style pt-2">
+							<h5 class="card-title fs-6 text-info">Editor</h5>
+              <p className="border-top border-2 border-dark text-danger py-0 my-1"><small>Golf Bangladesh</small></p>
+              <p className="border-top border-2 border-dark text-secondary py-0 mt-0"><small>News Letter, v.3</small></p>
+						</div>
+					</div>
+				</div>
+
+								
 			</div>
 		</div>
   );
